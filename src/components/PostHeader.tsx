9b53a6e41a04c80578292785117c30360eccd360
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2024-08-28 20:30:58 +0300

    Fix prettier config

--- src/components/PostHeader.tsx
+++ src/components/PostHeader.tsx
@@ -1,53 +1,53 @@
-"use client";
-import styles from "./PostHeader.module.css";
-import { TypeAnimation } from "react-type-animation";
+'use client';
 
-import { useState } from "react";
-import classNames from "classnames";
-import { sourceCodePro } from "@/app/fonts";
+import { useState } from 'react';
+
+import { sourceCodePro } from '@/app/fonts';
+import classNames from 'classnames';
+import { TypeAnimation } from 'react-type-animation';
+
+import styles from './PostHeader.module.css';
 
 export interface PostHeaderProps {
-  title: string;
-  description: string;
+	title: string;
+	description: string;
 }
 
 export const PostHeader = (props: PostHeaderProps) => {
-  const [isTyped, setIsTyped] = useState<boolean>(false);
-  return (
-    <div className={classNames(styles.container, sourceCodePro.className)}>
-      {isTyped ? (
-        <h1 className={styles.title}>
-          {props.title}
-          <span
-            style={{
-              width: "10px",
-              display: "inline-block",
-            }}
-          />
-        </h1>
-      ) : (
+	const [isTyped, setIsTyped] = useState<boolean>(false);
 
-
-        
-        <TypeAnimation
-          sequence={[
-            props.title,
-            () => {
-              setIsTyped(true);
-            },
-          ]}
-          wrapper="h1"
-          cursor
-          className={styles.title}
-        />
-      )}
-      <p
-        className={classNames(styles.subtitle, {
-          [styles.subtitleVisible]: isTyped,
-        })}
-      >
-        {props.description}
-      </p>
-    </div>
-  );
+	return (
+		<div className={classNames(styles.container, sourceCodePro.className)}>
+			{isTyped ? (
+				<h1 className={styles.title}>
+					{props.title}
+					<span
+						style={{
+							width: '10px',
+							display: 'inline-block',
+						}}
+					/>
+				</h1>
+			) : (
+				<TypeAnimation
+					sequence={[
+						props.title,
+						() => {
+							setIsTyped(true);
+						},
+					]}
+					wrapper="h1"
+					cursor
+					className={styles.title}
+				/>
+			)}
+			<p
+				className={classNames(styles.subtitle, {
+					[styles.subtitleVisible]: isTyped,
+				})}
+			>
+				{props.description}
+			</p>
+		</div>
+	);
 };
