b881eaf64fbc01d0b33fc1732b5f51209b17a0b8
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2024-09-03 22:42:29 +0300

    New favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,32 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { poppins } from "@/app/fonts";
-import { PageLayout } from "@/components/PageLayout";
-import { OverscrollFillerBottom } from "@/components/OverscrollFillerBottom";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
+import './globals.css';
+
+import type { Metadata } from 'next';
+
+import { poppins } from '@/app/fonts';
+import { OverscrollFillerBottom } from '@/components/OverscrollFillerBottom';
+import { PageLayout } from '@/components/PageLayout';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
-  title: "juiceo.me",
-  description: "Juuso's personal website",
+	title: 'juiceo.me',
+	description: "Juuso's personal website",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>
-        <SpeedInsights />
-        <Analytics />
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
+			<body className={poppins.className}>
+				<SpeedInsights />
+				<Analytics />
+				{children}
+			</body>
+		</html>
+	);
 }
