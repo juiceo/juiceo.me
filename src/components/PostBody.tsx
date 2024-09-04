17567f0aeee5d558fe2606918b022120d33bbcb7
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2024-09-04 14:51:00 +0300

    Refactor PostBody to styled-components

--- src/components/PostBody.tsx
+++ src/components/PostBody.tsx
@@ -1,26 +1,56 @@
-import { PropsWithChildren } from "react";
-import styles from "./PostBody.module.css";
-import { intlFormat, intlFormatDistance } from "date-fns";
+'use client';
+
+import { PropsWithChildren } from 'react';
+
+import { intlFormat } from 'date-fns';
+import styled from 'styled-components';
 
 export interface PostBodyProps {
-  publishedDate: string;
+	publishedDate: string;
 }
 
 export const PostBody = (props: PropsWithChildren<PostBodyProps>) => {
-  return (
-    <main className={styles.container}>
-      <p className={styles.publishedDate}>
-        {intlFormat(
-          props.publishedDate,
-          {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          },
-          { locale: "en-US" }
-        )}
-      </p>
-      <div className={styles.content}>{props.children}</div>
-    </main>
-  );
+	return (
+		<Container>
+			<PublishedDate>
+				{intlFormat(
+					props.publishedDate,
+					{
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+					},
+					{ locale: 'en-US' },
+				)}
+			</PublishedDate>
+			<Content>{props.children}</Content>
+		</Container>
+	);
 };
+
+const Container = styled.main`
+	transform: translateY(-150px);
+	width: 100%;
+	background: ${(props) => props.theme.colors.backgroundPage};
+	padding: 32px;
+	border-radius: 8px;
+	position: relative;
+	min-width: 0;
+
+	@media screen and (max-width: 900px) {
+		padding-left: 16px;
+		padding-right: 16px;
+	}
+`;
+
+const PublishedDate = styled.p`
+	color: ${(props) => props.theme.colors.textSecondary};
+	font-size: 1rem;
+	margin-bottom: 1rem;
+`;
+
+const Content = styled.div`
+	color: ${(props) => props.theme.colors.textBody};
+	font-size: 1rem;
+	line-height: 1.5rem;
+`;
